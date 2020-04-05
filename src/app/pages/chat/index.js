635f1d93ce089390729ee9a8f2c4f8da11da7849
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-04-06 00:33:30 +0100

    feat<parcial>: socket io connection with nodejs server

--- src/app/pages/chat/index.js
+++ src/app/pages/chat/index.js
@@ -5,12 +5,14 @@ import { FiLogOut, FiSend } from 'react-icons/fi';
 import socket from '../../services/socket';
 
 function Chat() {
-  const elements = [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3];
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
   const history = useHistory();
 
   useEffect(() => {
+    console.log('renderizando componente');
     socket.on('users', (socket_users) => {
       setUsers(socket_users);
     });
@@ -19,9 +21,28 @@ function Chat() {
     });
   });
 
+  useEffect(() => {
+    console.log(messages);
+    socket.on('chat', (socket_msg) => {
+      setMessages([...messages, ...[socket_msg]]);
+    });
+  });
+
   function disconnect() {
     socket.disconnect();
     history.goBack();
+  }
+
+  function send() {
+    if (message !== '') {
+      const msg = {
+        id: user.id,
+        name: user.name,
+        message,
+      };
+      setMessage('');
+      socket.emit('message', msg);
+    }
   }
 
 
@@ -31,26 +52,32 @@ function Chat() {
         <div className='conversation'>
           <div className='history'>
             {
-              elements.map((element, index) => {
-                if (element === 1) {
+              messages.map((element, index) => {
+                if (element.id === user.id) {
                   return (
-                    <div className='message right-message'>
-                      <h5>Fernando said:</h5>
-                      <h6>
-                        {index}
+                    // eslint-disable-next-line react/no-array-index-key
+                    <div key={index} className='message right-message'>
+                      <h5>
+                        {element.name}
                         {' '}
-                        - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        said:
+                      </h5>
+                      <h6>
+                        {element.message}
                       </h6>
                     </div>
                   );
                 }
                 return (
-                  <div className='message'>
-                    <h5>Fernando said:</h5>
-                    <h6>
-                      {index}
+                  // eslint-disable-next-line react/no-array-index-key
+                  <div key={index} className='message'>
+                    <h5>
+                      {element.name}
                       {' '}
-                      - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      said:
+                    </h5>
+                    <h6>
+                      {element.message}
                     </h6>
                   </div>
                 );
@@ -58,8 +85,14 @@ function Chat() {
             }
           </div>
           <div className='input'>
-            <input type='text' placeholder='Type a message' />
-            <button type='button'>
+            <input
+              type='text'
+              placeholder='Type a message'
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+              onSubmit={(event) => setMessage(event.target.value)}
+            />
+            <button type='button' onClick={send}>
               <FiSend size={20} />
               &nbsp;
             </button>
